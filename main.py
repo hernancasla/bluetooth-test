08d86d13ddd8ef3a9e457fafeba0c401ab49c5b1
sms-hceballos <hceballos@sms-sudamerica.com>
2020-12-30 17:24:55 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import bluetooth
+
+print("Performing inquiry...")
+
+nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True,
+                                            flush_cache=True, lookup_class=False)
+
+print("Found {} devices".format(len(nearby_devices)))
+for device in nearby_devices:
+    try:
+        print("   {} - {}".format(device.address, device.name))
+    except UnicodeEncodeError:
+        print("   {} - {}".format(device.address, device.name.encode("utf-8", "replace")))
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
